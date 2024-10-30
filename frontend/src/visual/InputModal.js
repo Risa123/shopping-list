ae7fae1d41570e9eb45624251051bd6decba71f4
Risa123 <risamouris@gmail.com>
2024-10-30 23:35:32 +0100

    commit 1

--- frontend/src/visual/InputModal.js
+++ frontend/src/visual/InputModal.js
@@ -1,12 +1,17 @@
 import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
 
 export default function InputModal(props){
-    return <Modal show = {props.show} onHide = {()=>props.setShow(false)}>
-       <Modal.Header closeButton>
+    return <Modal.Dialog show = {props.show}>
+       <Modal.Header>
           <Modal.Title>{props.title}</Modal.Title>
        </Modal.Header>
        <Modal.Body>
          test
        </Modal.Body>
-    </Modal>
+       <Modal.Footer>
+         <Button variant = "secondary" onClick={()=>props.setShow(false)}>Close</Button>
+         <Button variant = "primary" onClick = {()=>props.setShow(false)}>Ok</Button>
+       </Modal.Footer>
+    </Modal.Dialog>
 }
